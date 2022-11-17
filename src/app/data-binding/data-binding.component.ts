8d8a5af7e25a2dd43b197a1bdb18d64dfde2e08e
Leonardo Lima Coelho <dev.leonardolima@gmail.com>
2022-11-17 14:42:42 -0400

    Aula 10: Class e Style Binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [
+  {
+    type: 'success',
+    message: 'Esse é um alerta de sucesso',
+  },
+  {
+    type: 'info',
+    message: 'Esse é um alerta de informe',
+  },
+  {
+    type: 'warning',
+    message: 'Esse é um alerta de aviso',
+  },
+  {
+    type: 'danger',
+    message: 'Esse é um alerta de perigo',
+  },
+  {
+    type: 'primary',
+    message: 'Esse é um alerta primário',
+  },
+  {
+    type: 'secondary',
+    message: 'Esse é um alerta secundário',
+  },
+  {
+    type: 'light',
+    message: 'Esse é um alerta claro',
+  },
+  {
+    type: 'dark',
+    message: 'Esse é um alerta escuro',
+  },
+];
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss'],
+})
+export class DataBindingComponent implements OnInit {
+  name: string;
+  url: string;
+  cursoAngular: boolean;
+  urlImagem: string;
+  isOpen: boolean;
+  typeAlert:
+    | 'success'
+    | 'info'
+    | 'warning'
+    | 'danger'
+    | 'primary'
+    | 'secondary'
+    | 'light'
+    | 'dark'
+    | undefined;
+
+  alerts: Alert[];
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  }
+
+  getValor(value: number) {
+    return value;
+  }
+  getCurtirCurso() {
+    return true;
+  }
+  constructor() {
+    this.name = 'Leonardo';
+    this.url = 'https://leonardo.dev/';
+    this.cursoAngular = true;
+    this.urlImagem = 'http://lorempixel.com.br/300/200/';
+
+    this.isOpen = true;
+
+    this.alerts = Array.from(ALERTS);
+    this.reset();
+  }
+
+  ngOnInit(): void {}
+}
